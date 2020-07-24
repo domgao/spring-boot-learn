d343a7d836697460b298facacaa578aac7599d6e
domgao <1010663904@qq.com>
2020-07-24 14:38:24 +0800

    commit

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-springapplication/src/main/java/smoketest/springapplication/SampleSpringApplication.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-springapplication/src/main/java/smoketest/springapplication/SampleSpringApplication.java
@@ -1,4 +1,12 @@
 package smoketest.springapplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SampleSpringApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringApplication.class, args);
+	}
 }
